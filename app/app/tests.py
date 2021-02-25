56ec8e21dfc5899184e1783a6e3613b799f3d329
frank llovet <frank@llovet.cat>
2021-02-25 16:39:32 +0100

    test

---
+++ app/app/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+#from app.calc import add
+from app.calc import add
+
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """Test that values are added together"""
+        self.assertEqual(add(3, 8), 11)
